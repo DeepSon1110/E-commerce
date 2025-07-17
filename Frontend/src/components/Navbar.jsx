bee18c0c7971ebf3791c195f46f4aaa3c77d1500
Dip_San <khadkasandip489@gmail.com>
2025-07-17 10:46:17 +0545

    feat: Update Navbar styling and enhance Login form with submit functionality

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-   <nav className='flex gap-5 border-4 border-gray-500 px-3'>
+   <nav className='flex md:justify-center justify-between lg:justify-around border-2 border-gray-500 px-6 flex-wrap bg-red-500 md:bg-green-400'>
     <Link to= {"/login"}>Login</Link>
     <Link to= {"/"}>Home</Link>
     <Link to= {"/register"}>Register</Link>
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div>Login</div>
+      <form onSubmit = {handleSubmit}> 
+        <div>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id= "email" placeholder= "example@gmai.com" />
+        </div>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id= "password" placeholder= "***********"/>
+        </div>
+        <button type = "submit">submit</button>
+      </form>
+    </>
+  );
+};
 
-export default Login
+export default Login;
