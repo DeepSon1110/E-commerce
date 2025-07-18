82b009449fade1d9a35d578dea990e0363083a42
Dip_San <khadkasandip489@gmail.com>
2025-07-18 10:13:13 +0545

    feat: Update Navbar styling and enhance Login form with submit functionality

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,23 +1,55 @@
-import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); // <-- Add this
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle login here
+  };
+
+  console.log("email", email);
+  console.log("password", password);
+
   return (
     <>
-      <div>Login</div>
-      <form onSubmit = {handleSubmit}> 
+      <div className="h-screen flex flex-col items-center justify-center">
+        <div>Login</div>
         <div>
-            <label htmlFor="email">Email:</label>
-            <input type="text" id= "email" placeholder= "example@gmai.com" />
+          <form
+            onSubmit={handleSubmit}
+            className="p-4 border flex flex-col gap-4 items-start"
+          >
+            {/* Email */}
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                placeholder="example@gmail.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            {/* Password */}
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            {/* Submit button */}
+            <button type="submit">Submit</button>
+          </form>
         </div>
-        <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id= "password" placeholder= "***********"/>
-        </div>
-        <button type = "submit">submit</button>
-      </form>
+      </div>
     </>
   );
 };
